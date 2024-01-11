5f2bdc5d914dd6056792fad44fde93fa8879dd91
failtehu <failtehu@zego.im>
2024-01-11 15:46:08 +0800

    feat: add 11

--- 11.盛最多水的容器.ts
+++ 11.盛最多水的容器.ts
@@ -6,7 +6,22 @@
 
 // @lc code=start
 function maxArea(height: number[]): number {
-    
+    let first = 0
+    let last = height.length - 1
+    let maxSquare = 0
+    const calcSquara = (start, end) => {
+        const minHeight = Math.min(height[start], height[end])
+        const width = Math.abs(end - start)
+        return minHeight * width
+    }
+    while (first < last) {
+        maxSquare = Math.max(calcSquara(first, last), maxSquare)
+        if (height[first] < height[last]) {
+            first++
+        } else {
+            last--
+        }
+    }
+    return maxSquare
 };
 // @lc code=end
-
