2684a19699f703e44999759209ea704ffdcbb459
hn-failte <failte@163.com>
2024-01-18 22:04:16 +0800

    feat: add 509、322

--- 322.零钱兑换.ts
+++ 322.零钱兑换.ts
@@ -6,7 +6,20 @@
 
 // @lc code=start
 function coinChange(coins: number[], amount: number): number {
-    
+    let dp = new Array(amount + 1).fill(amount + 1);
+    dp[0] = 0;
+    // i: 面额
+    for (let i = 1; i <= amount; i++) {
+        // j: 硬币序数
+        for (let j = 0; j < coins.length; j++) {
+            if (coins[j] <= i) {
+                // 第i个的最少数 = dp[i] 和 第i-coins[j]的最少数 + 1的最小值
+                // 加遍历，即所有硬币中的最少的那个
+                // 从小到大可以确保已计算的最少始终是正确的
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    return dp[amount] > amount ? -1 : dp[amount];
 };
 // @lc code=end
-
--- 509.斐波那契数.ts
+++ 509.斐波那契数.ts
@@ -6,7 +6,16 @@
 
 // @lc code=start
 function fib(n: number): number {
-
+    if (n < 2) return n
+    let c = 1 // n
+    let p = 0 // n - 1
+    let q = 0 // n - 2
+    for(let i = 2; i <= n; i++) {
+        q = p
+        p = c
+        // f(n) = f(n - 1) + f(n - 2)
+        c = p + q
+    }
+    return c
 };
 // @lc code=end
-
