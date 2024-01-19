ca1416399cb236325819d430e24b9d5182a8d77e
failtehu <failtehu@zego.im>
2024-01-19 14:34:20 +0800

    refactor: 1、26

--- 1.两数之和.ts
+++ 1.两数之和.ts
@@ -4,23 +4,17 @@
  * [1] 两数之和
  */
 
-// @lc code=start
-// function twoSum(nums: number[], target: number): number[] {
-//     for (let m = 0; m < nums.length - 1; m++) {
-//         for (let n = m + 1; n < nums.length; n++) {
-//           if (nums[m] + nums[n] === target) {
-//             return [m, n];
-//           }
-//         }
-//     }
-// };
 function twoSum(nums: number[], target: number): number[] {
-    let map = new Map();
-    for (let m = 0; m < nums.length; m++) {
-      const item = nums[m];
-      const current = map.get(target - item);
-      if (current > -1) return [current, m];
-      map.set(item, m);
+  const cache = new Array()
+  let list: number[] = []
+  for (let i = 0; i < nums.length; i++) {
+    if (cache[nums[i]] !== void 0) {
+      list = [i, cache[nums[i]]]
+      break
+    } else {
+      cache[target - nums[i]] = i
     }
+  }
+  return list
 };
 // @lc code=end
