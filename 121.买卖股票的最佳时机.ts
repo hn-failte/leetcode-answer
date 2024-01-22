90afc0c53985fdab3c9d3e98a9c29de2393c2304
failtehu <failtehu@zego.im>
2024-01-22 11:55:59 +0800

    feat: add 121、122

--- 121.买卖股票的最佳时机.ts
+++ 121.买卖股票的最佳时机.ts
@@ -6,7 +6,16 @@
 
 // @lc code=start
 function maxProfit(prices: number[]): number {
-    
+    let minprice = Number.MAX_SAFE_INTEGER; // 最低价
+    let maxprofit = 0; // 最大利润
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minprice) {
+            minprice = prices[i];
+        } else {
+            maxprofit = Math.max(maxprofit, prices[i] - minprice)
+        }
+    }
+    return maxprofit;
+
 };
 // @lc code=end
-
