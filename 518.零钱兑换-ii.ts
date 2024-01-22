7524491163d82f9bf693b008e499249ec5072b1a
hn-failte <failte@163.com>
2024-01-22 22:38:12 +0800

    feat: add 518

--- 518.零钱兑换-ii.ts
+++ 518.零钱兑换-ii.ts
@@ -6,7 +6,13 @@
 
 // @lc code=start
 function change(amount: number, coins: number[]): number {
-    
+    const dp = new Array(amount + 1).fill(0)
+    dp[0] = 1
+    for (let coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin]
+        }
+    }
+    return dp[amount]
 };
 // @lc code=end
-
