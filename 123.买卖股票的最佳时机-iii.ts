b62d56348c2b6cb4d3cb25043c807cbff6e09ea0
failtehu <failtehu@zego.im>
2024-01-22 18:03:19 +0800

    feat: add 123

--- 123.买卖股票的最佳时机-iii.ts
+++ 123.买卖股票的最佳时机-iii.ts
@@ -6,7 +6,16 @@
 
 // @lc code=start
 function maxProfit(prices: number[]): number {
-    
+    const n = prices.length;
+    let min = prices[0]
+    let offset = prices[0]
+    let profile1 = 0, profile2 = 0
+    for (let i = 1; i < n; i++) {
+        min = Math.min(min, prices[i])
+        profile1 = Math.max(profile1, prices[i] - min)
+        offset = Math.min(offset, prices[i] - profile1)
+        profile2 = Math.max(profile2, prices[i] - offset)
+    }
+    return profile2
 };
 // @lc code=end
-
